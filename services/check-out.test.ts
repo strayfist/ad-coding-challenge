9abb581ac901191448bb659007429156351ecab3
David Chiew <david@sabrecreations.com>
2023-01-22 00:28:29 +1100

    Support for carts with a single item, added unit tests for each product type

--- services/check-out.test.ts
+++ services/check-out.test.ts
@@ -1,10 +1,25 @@
-import { Checkout } from "./check-out"
+import { Checkout, ProductType } from "./check-out";
 
 describe("Checkout", () => {
-	describe("total", () => {
-		test("no items in cart should be zero total", () => {
-			const checkout = new Checkout();
-			expect(checkout.total()).toEqual(0);
-		})
-	})
-})
+  describe("total", () => {
+    test("no items in cart should be $0", () => {
+      const checkout = new Checkout();
+      expect(checkout.total()).toEqual(0);
+    });
+
+    const expectedPricings = [
+      { productType: ProductType.ClassicAd, expectedPrice: 269.99 },
+      { productType: ProductType.StandoutAd, expectedPrice: 322.99 },
+      { productType: ProductType.PremiumAd, expectedPrice: 394.99 },
+    ];
+    describe("single product in cart has correct pricing", () => {
+      expectedPricings.forEach(({ productType, expectedPrice }) => {
+        test(`${productType} has correct price $${expectedPrice}`, () => {
+          const checkout = new Checkout();
+          checkout.add({ productType });
+          expect(checkout.total()).toEqual(expectedPrice);
+        });
+      });
+    });
+  });
+});
--- services/check-out.ts
+++ services/check-out.ts
@@ -1,7 +1,35 @@
 export type PricingRule = {};
+export type Product = {
+  productType: ProductType;
+};
+export enum ProductType {
+  ClassicAd = "ClassicAd",
+  StandoutAd = "StandoutAd",
+  PremiumAd = "PremiumAd",
+}
+
+const productPricing: Record<ProductType, number> = {
+  ClassicAd: 269.99,
+  StandoutAd: 322.99,
+  PremiumAd: 394.99,
+};
 
 export class Checkout {
-	constructor(private pricingRules: PricingRule[] = []) { }
-	
-	public total = () => 0;
+  private cart: Product[] = [];
+
+  constructor(private pricingRules: PricingRule[] = []) {}
+
+  public add = (product: Product) => {
+    this.cart.push(product);
+  };
+
+  public total = () => {
+    if (this.cart.length === 0) {
+      return 0;
+    }
+
+    const firstProduct = this.cart[0];
+    const price = productPricing[firstProduct.productType];
+    return price;
+  };
 }
